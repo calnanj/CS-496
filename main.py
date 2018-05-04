17a8fa6efc1cbe925e7b073154b5b76a31487537
calnanj <calnanj@oregonstate.edu>
2018-05-04 08:11:41 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
+import webapp2
+import urllib
+import random
+import string
+import json
+import os
+
+CLIENT_ID = '583327101067-jg52piaabogii6nn5khl6dnvl07c4304.apps.googleusercontent.com'
+CLIENT_SECRET = '_BCTHMkRM4EX1eUCeIcypcQe'
+REDIRECT_URI = 'https://oauth2.appspot.com/oauth'
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        random_string = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+
+        url_linktext = 'Provide Access'
+
+        url = "https://accounts.google.com/o/oauth2/v2/auth?"
+        url = url + "scope=email"
+        url = url + "&access_type=offline"
+        url = url + "&include_granted_scopes=true"
+        url = url + "&state="
+        url = url + random_string
+        url = url + "&redirect_uri=https://oauth2.appspot.com/oauth"
+        url = url + "&response_type=code"
+        url = url + "583327101067-jg52piaabogii6nn5khl6dnvl07c4304.apps.googleusercontent.com"
+
+        template_values = {'url': url}
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        self.response.out.write(template.render(path, template_values))
+
+class OAuthHandler(webapp2.RequestHandler):
+    def get(self):
+        auth_code = self.request.GET['code']
+        state = self.request.GET['state']
+        post_body = {
+            'code': auth_code,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'redirect_uri': REDIRECT_URI,
+            'grant_type': 'authorization_code'
+            }
+
+        payload = urllib.urlencode(post_body)
+        headers = {'Content-Type':'application/x-www-form-urlencoded'}
+        result = urlfetch.fetch(
+            url="https://www.googleapis.com/oauth2/v4/token",
+   		 	payload = payload,
+    		method = urlfetch.POST,
+    		headers = headers)
+
+        json_result = json.loads(result.content)
+
+        headers = {'Authorization': 'Bearer ' + json_result['access_token']}
+        result = urlfetch.fetch(
+            url="https://www.googleapis.com/plus/v1/people/me",
+            method = urlfetch.GET,
+            headers=headers)
+
+        json_result = json.loads(result.content)
+        # self.response.write(json.dumps(json_result['name']['givenName']))
+        exist_fname = False
+        exist_lname = False
+        exist_gplink = False
+        exist_account = False
+        for item in json_result:
+            if item == 'name':
+                if item[0]:
+                    exist_fname = True
+                if item[0]:
+                    exist_lname = True
+            if item == 'url':
+                exist_gplink = True
+
+        if exist_fname and exist_lname and exist_gplink:
+            fname = json_result['name']['givenName']
+            lname = json_result['name']['familyName']
+            gplink = str(json_result['url'])
+            exist_account = True
+            template_values = {'fname': fname,
+                               'lname': lname,
+                               'gplink': gplink,
+                               'gplink_name': "Visit Profile",
+                               'state': state}
+        else:
+            template_values = {'noAccount': "NOTE: No Google+ account found", 'state': state}
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/oauth.html')
+        self.response.out.write(template.render(path, template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/oauth',OAuthHandler)
+], debug=True)
